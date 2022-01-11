b724304c3bbbffd1065e37e1d85c159a18b4b1a4
AlanSousacc <alansousa.cc@gmail.com>
2022-01-11 09:36:25 -0300

    Ajuste readme.md

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,9 +2,8 @@
 
 namespace Database\Seeders;
 
-use App\Models\Customer;
-use App\Models\Number;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class DatabaseSeeder extends Seeder
 {
@@ -15,6 +14,12 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+        DB::statement('SET FOREIGN_KEY_CHECKS=0');
+
+        DB::table('users')->truncate();
+        
+        $this->call([UserTableSeeder::class]);
+
         \App\Models\Customer::factory()->count(10)->create()->each(function($customer) {
 
             $number = \App\Models\Number::factory()->make();
